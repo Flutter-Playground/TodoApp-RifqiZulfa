5b2ade943ed3835e406c529bfbeaca176b86be5e
rzs04 <facet076@gmail.com>
2021-12-31 15:29:06 +0700

    todo app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_app/screens/note_list.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: NoteList(),
+    );
+  }
+}
---
+++ lib/model.dart
@@ -0,0 +1,8 @@
+class Note {
+  String title;
+  Note({required this.title});
+  factory Note.fromJson(Map < String, dynamic > json) => Note(
+    title: json['title']
+  );
+  Map < String, dynamic > toJson() => {'title': title};
+}
